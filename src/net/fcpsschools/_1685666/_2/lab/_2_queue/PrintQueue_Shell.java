0df46e77da19d562721694eab411e1e3cdaa501b
ApolloZhu <public-apollonian@outlook.com>
2017-11-28 22:24:49 -0500

    Queue Lab w\ Comments

--- src/net/fcpsschools/_1685666/_2/lab/_2_queue/PrintQueue_Shell.java
+++ src/net/fcpsschools/_1685666/_2/lab/_2_queue/PrintQueue_Shell.java
@@ -4,29 +4,26 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
+/**
+ * @author Zhiyu Zhu, Pd. 1
+ * How I feel: the delete should be FIFO, not assigned.
+ */
 public class PrintQueue_Shell {
     private static final Scanner in = new Scanner(System.in);
     private static final Queue<String> q = new LinkedList<>();
     private static int jobNumber = 100;
 
     public static void main(String[] args) throws Exception {
-        String prompt = "Add, Print, Delete, eXit: ";
+        String str, prompt = "Add, Print, Delete, eXit: ";
         System.out.print(prompt);
-        String str = in.next();
-        while (!str.equals("X")) {
-            if (str.equals("A"))
-                add();
-            else if (str.equals("P"))
-                printJob();
-            else if (str.equals("D"))
-                delete();
-            else
-                System.out.println("invalid command");
+        while (!(str = in.next()).equals("X")) {
+            if (str.equals("A")) add();
+            else if (str.equals("P")) printJob();
+            else if (str.equals("D")) delete();
+            else System.out.println("invalid command");
             printQueue();
             System.out.print(prompt);
-            str = in.next();
         }
-        in.close();
     }
 
     public static void add() {
@@ -34,18 +31,41 @@ public class PrintQueue_Shell {
     }
 
     public static void printJob() {
+        // Do nothing
     }
 
+    // Although it is required to behave like this
+    // I don't feel like this is an queue behavior
     public static void delete() {
         System.out.print("Enter job number: ");
         String next = in.next();
-        if (q.remove(next))
-            System.out.println("Deleted job " + next);
-        else
-            System.out.println("Error--job does not exist");
+        if (q.remove(next)) System.out.println("Deleted job " + next);
+        else System.out.println("Error--job does not exist");
     }
 
     public static void printQueue() {
         System.out.println(q);
     }
 }
+
+/*
+Add, Print, Delete, eXit: A
+[100]
+Add, Print, Delete, eXit: A
+[100, 101]
+Add, Print, Delete, eXit: A
+[100, 101, 102]
+Add, Print, Delete, eXit: P
+[100, 101, 102]
+Add, Print, Delete, eXit: A
+[100, 101, 102, 103]
+Add, Print, Delete, eXit: D
+Enter job number: 105
+Error--job does not exist
+[100, 101, 102, 103]
+Add, Print, Delete, eXit: D
+Enter job number: 102
+Deleted job 102
+[100, 101, 103]
+Add, Print, Delete, eXit: X
+*/
