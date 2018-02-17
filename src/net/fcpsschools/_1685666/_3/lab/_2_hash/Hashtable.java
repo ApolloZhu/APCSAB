fae686d0c904bafc1258b801b4a110dff758a46b
ApolloZhu <public-apollonian@outlook.com>
2018-02-17 10:51:58 -0500

    Hash - CHAINING

--- src/net/fcpsschools/_1685666/_3/lab/_2_hash/Hashtable.java
+++ src/net/fcpsschools/_1685666/_3/lab/_2_hash/Hashtable.java
@@ -1,5 +1,7 @@
 package net.fcpsschools._1685666._3.lab._2_hash;
 
+import java.util.Objects;
+
 /*****************************************************************************
  Assignment: Notice that collisions occur. You are to implement
  three different collision schemes, namely, linear probing,
@@ -9,66 +11,62 @@ package net.fcpsschools._1685666._3.lab._2_hash;
 
  For separate chaining,if you want to use a table of LinkedList objects, it is fine with me.
  *****************************************************************************/
-public abstract class Hashtable<E> {
+public abstract class Hashtable {
+    protected Object[] table;
 
-    private int size;
-    private boolean useProbing;
-    private E[] table;
+    protected Hashtable(int size) {
+        this.table = new Object[size];
+    }
 
-    public static <E> Hashtable<E> init(int numSlots, boolean useProbing) {
+    public static Hashtable init(int numSlots, boolean useProbing) {
         return null;
     }
 
-    protected Hashtable(E[] table) {
-        this.table = table;
+    /**
+     * Rehash
+     */
+    protected int index(Object obj) {
+        obj = Objects.requireNonNull(obj);
+        int hash = Math.abs(obj.hashCode());
+        return hash % table.length;
     }
 
-    public void add(E obj) {
-        int code = Math.abs(obj.hashCode());
-        System.out.println("Hash Code: " + code);
-        int index = code % size;
+    public void add(Object obj) {
+        if (null == obj) return;
+        int index = index(obj);
         if (null == table[index]) {
             table[index] = obj;
-        } else {
-            if (useProbing) { /*Linear Probing*/
-                // TODO: call the linearProbe method
-            } else { /*Chaining*/
-                // TODO: call the chaining method
-            }
-        }
+        } else resolve(obj, index);
+        //if (useProbing) { /*Linear Probing*/
+        //    TODO: call the linearProbe method
+        //}
     }
 
-    // returns true if obj can be found in the table
-    public boolean contains(E obj) {
-        int code = Math.abs(obj.hashCode());
-        int index = code % size;
+    protected abstract void resolve(Object obj, int index);
 
-        if (useProbing) {
-            /*Linear Probing*/
-        } else {
-            /*Chaining*/
-        }
-        return true;
-    }//contains
+    /**
+     * @return true if obj can be found in the table
+     * if (useProbing) { Linear Probing }
+     * else { Chaining }
+     */
+    public boolean contains(Object obj) {
+        if (null == obj) return false;
+        int index = index(obj);
+        if (null == table[index]) return false;
+        return obj.equals(table[index]) ||
+                containsResolved(obj, index);
+    }
 
     // TODO: implement collision resolution methods one at a time
+    protected abstract boolean containsResolved(Object obj, int index);
 
     /**
      * @param index
      * @return
      * @apiNote implement this method recursively
      */
-    public int linearProbe(int index) {
+    private int linearProbe(int index) {
         return 0;
-    }
-
-    /**
-     * One student asked me what the method chaining returns. It returns the head of the new linked list (each table entry is a singly linked list of ListNodes) so that you can do:    table[index] = chaining (index, obj).
-     *
-     * @apiNote each table element is a singly linked list of Objects
-     */
-    public E chaining(int index, E obj) {
-        return null;
     }
 
     public String toString() {
@@ -81,10 +79,10 @@ public abstract class Hashtable<E> {
         }
         sb.append(table[i]);
         sb.append(']');
-        return null;
+        return sb.toString();
     }
 
     public enum CollisionResolutionMethod {
-        LINEAR_PROBING, QUADRATIC_PROBING, CHAINING, REHASHING
+        LINEAR_PROBING, QUADRATIC_PROBING, CHAINING
     }
 }
---
+++ src/net/fcpsschools/_1685666/_3/lab/_2_hash/LinkedHashTable.java
@@ -0,0 +1,48 @@
+package net.fcpsschools._1685666._3.lab._2_hash;
+
+
+public class LinkedHashTable extends Hashtable {
+    public LinkedHashTable(int size) {
+        super(size);
+    }
+
+    @Override
+    protected void resolve(Object obj, int index) {
+        if (table[index] instanceof ListNode) {
+            table[index] = new ListNode(obj, (ListNode) table[index]);
+        } else {
+            ListNode previous = new ListNode(table[index], null);
+            table[index] = new ListNode(obj, previous);
+        }
+    }
+
+    @Override
+    public boolean containsResolved(Object obj, int index) {
+        if (table[index] instanceof ListNode) {
+            ListNode node = (ListNode) table[index];
+            do {
+                if (obj.equals(node.value))
+                    return true;
+            } while (null != (node = node.next));
+            return false;
+        } else return false;
+    }
+
+    private static class ListNode {
+        private final Object value;
+        private final ListNode next;
+
+        private ListNode(Object initValue, ListNode initNext) {
+            value = initValue;
+            next = initNext;
+        }
+
+        @Override
+        public String toString() {
+            String rest = null == next ? ""
+                    : "-" + next.toString();
+            return String.valueOf(value) + rest;
+        }
+    }
+}
+
